c0492d9ee5766c5e82d2b981aa11e129e7251885
seoyeon08 <silversun0808@gmail.com>
2024-04-30 14:56:18 +0900

    Feat: User entity, repository, dto, service, controller

---
+++ kiosk-back/src/main/java/com/study/kioskback/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.study.kioskback.controller;
+
+import com.study.kioskback.dto.UserSaveDto;
+import com.study.kioskback.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/user")
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/point")
+    public void checkUserPoint(@RequestBody UserSaveDto dto,
+                                 @RequestParam("userPhoneNumber") String userPhoneNumber,
+                                 @RequestParam("productPrice") int productPriceSum){
+        // productPrice를 총 주문 금액으로 변경 필요.
+        int point = productPriceSum / 100;
+        userService.updatePoint(dto, point);        // 적립
+    }
+}
