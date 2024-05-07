8a084964906ae2ea285eb4471b6597554a589919
kwang <shinkwang@yonsei.ac.kr>
2024-05-07 17:30:12 +0900

    feat: Add File upload feature

---
+++ kiosk-back/src/main/java/com/study/kioskback/util/FileStore.java
@@ -0,0 +1,49 @@
+package com.study.kioskback.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String fileName) {
+        return fileDir + fileName;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFileName = createStoreFileName(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        return new UploadFile(originalFilename, storeFileName);
+    }
+
+    public boolean deleteFile(String storeFileName) {
+        File file = new File(getFullPath(storeFileName));
+        if (file.exists()) {
+            return file.delete();
+        }
+        return false;
+    }
+
+    private String createStoreFileName(String originalFileName) {
+        String ext = extractExt(originalFileName);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String originalFileName) {
+        int pos = originalFileName.lastIndexOf(".");
+        return originalFileName.substring(pos + 1);
+    }
+}
