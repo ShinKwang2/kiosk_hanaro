efc058af6764aaf8ce67d56aef96dfd1d92c0de0
kwang <shinkwang@yonsei.ac.kr>
2024-05-08 14:03:52 +0900

    feat: Make ErrorResponse

--- kiosk-back/src/main/java/com/study/kioskback/error/ExceptionController.java
+++ kiosk-back/src/main/java/com/study/kioskback/error/ExceptionController.java
@@ -1,7 +1,27 @@
 package com.study.kioskback.error;
 
+import com.study.kioskback.api.ApiResponse;
+import com.study.kioskback.error.exception.CustomException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(CustomException.class)
+    public ApiResponse<Object> customException(CustomException e) {
+        return ApiResponse.of(HttpStatus.resolve(e.getStatusCode()),
+                e.getMessage(), null);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public ApiResponse<Object> bindException(BindException e) {
+        return ApiResponse.of(HttpStatus.BAD_REQUEST,
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                null);
+    }
 }
